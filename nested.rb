2692447f13e2c9dc11dbcb152b5682eeddbf52a8
tgray017 <tgray017@gmail.com>
2018-10-05 22:21:04 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,8 +87,10 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto] = :known_for
-    
+  programmer_hash[:yukihiro_matsumoto] = :known_for, :languages
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["Lisp", "C"]  
+   
 end
 
 def changing_alan
